38f82dadd9a217d059caeeaecffb9bac0e59e538
创新共赢 <139563281@qq.com>
2024-06-18 07:12:45 +0800

    Signed-off-by: 创新共赢 <139563281@qq.com>注释掉type="bedrock-model"，原方法中无此参数

--- salesgpt/parsers.py
+++ salesgpt/parsers.py
@@ -7,10 +7,26 @@ from langchain.schema import AgentAction, AgentFinish  # OutputParserException
 
 
 class SalesConvoOutputParser(AgentOutputParser):
+    """
+    SalesConvoOutputParser 是 SalesConvo 代理输出的解析器。
+
+    Args:
+        AgentOutputParser (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
     ai_prefix: str = "AI"  # change for salesperson_name
     verbose: bool = False
 
-    def get_format_instructions(self) -> str:
+    def get_format_instructions(self):
+        """
+        获取代理的格式说明。
+
+        Returns:
+            str: 代理的格式说明.
+        """
         return FORMAT_INSTRUCTIONS
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
@@ -29,5 +45,8 @@ class SalesConvoOutputParser(AgentOutputParser):
         return AgentAction(action.strip(), action_input.strip(" ").strip('"'), text)
 
     @property
-    def _type(self) -> str:
+    def _type(self):
+        """
+        Returns: str
+        """
         return "sales-agent"
